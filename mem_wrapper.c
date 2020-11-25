5e10e86aa9828c3402f861f8ad99c5483a19303b
Tian Zeng <tianyu_zeng@apple.com>
2020-11-24 20:37:49 -0500

    add malloc test and fix issues discovered by test

--- mem_wrapper.c
+++ mem_wrapper.c
@@ -5,34 +5,51 @@
 #include <string.h>
 #include <math.h>
 
-#define ALLOCATION_BUCKET_COUNT (12)
-#define ALLOCATION_BUCKET_OFFSET (2)
-#define TIME_BUCKET_COUNT (5)
-
-// global statistics
-typedef struct {
-    uint64_t num_allocations; // how many allocations have been made total
-    uint64_t current_allocated_size; // how much memory have we currently allocated
-    uint64_t allocation_buckets[ALLOCATION_BUCKET_COUNT]; // how much memory was allocated
-    uint64_t time_buckets[TIME_BUCKET_COUNT]; // how much time is memory spent allocated
-} mem_stats_t;
-
-// created each time memory is allocated, removed each time memory is freed
-typedef struct mem_info {
-    void * ptr;
-    uint64_t time_allocated;
-    uint64_t size_allocated;
-    struct mem_info *next;
-} mem_info_t;
 
 static mem_stats_t memstats;
-static time_t print_time = 0;
 
 static mem_info_t *head = NULL; // linked list so we can store as many of these as needed
 
 void stats_init(void) {
     memset(&memstats, 0, sizeof(mem_stats_t));
-    print_time = time(0);
+}
+
+static void populate_time_buckets(void) {
+    memset(&memstats.time_buckets, 0, sizeof(uint64_t)*TIME_BUCKET_COUNT);
+
+    // nothing to populate
+    if(head == NULL) {
+        return;
+    }
+
+    memset(&memstats.time_buckets, 0, sizeof(uint64_t)*TIME_BUCKET_COUNT);
+    time_t current_time = time(0);
+
+    mem_info_t *alias = head;
+    // add time data to correct bucket
+    for(int i = 0; i < TIME_BUCKET_COUNT; i++) {
+        if((int)difftime(current_time, alias->time_allocated) < pow(10,i)) {
+            memstats.time_buckets[i]++;
+            break;
+        }
+    }
+    while(alias->next != NULL) {
+        // move on to next mem block
+        alias = alias->next;
+
+        // add time data to correct bucket
+        for(int i = 0; i < TIME_BUCKET_COUNT; i++) {
+            if((int)difftime(current_time, alias->time_allocated) < pow(10,i)) {
+                memstats.time_buckets[i]++;
+                break;
+            }
+        }
+    }
+}
+
+mem_stats_t get_mem_stats(void) {
+    populate_time_buckets();
+    return memstats;
 }
 
 // Thread to print out statistics every 5 seconds
@@ -62,33 +79,11 @@ void print_thread(void){
     // time must be generated on the spot to be accurate
     // iterate through linked list to get the correct time buckets
     fprintf(stderr, "\nCurrent Allocations by Age\n");
-    memset(&memstats.time_buckets, 0, sizeof(uint64_t)*TIME_BUCKET_COUNT);
-    print_time = time(0);
-    if(head != NULL) {
-        mem_info_t *alias = head;
-        // add time data to correct bucket
-        for(int i = 0; i < TIME_BUCKET_COUNT; i++) {
-            if((int)difftime(print_time, alias->time_allocated) < pow(10,i)) {
-                memstats.time_buckets[i]++;
-                break;
-            }
-        }
-        while(alias->next != NULL) {
-            // move on to next mem block
-            alias = alias->next;
 
-            // add time data to correct bucket
-            for(int i = 0; i < TIME_BUCKET_COUNT; i++) {
-                if((int)difftime(print_time, alias->time_allocated) < pow(10,i)) {
-                    memstats.time_buckets[i]++;
-                    break;
-                }
-            }
-        }
-        // Print out numbers
-        for(int i = 0; i < TIME_BUCKET_COUNT; i++) {
-            fprintf(stderr, "<%d seconds: %llu\n", (int)pow(10,i), memstats.time_buckets[i]);
-        }
+    // Print out numbers
+    populate_time_buckets();
+    for(int i = 0; i < TIME_BUCKET_COUNT; i++) {
+        fprintf(stderr, "<%d seconds: %llu\n", (int)pow(10,i), memstats.time_buckets[i]);
     }
 
      fprintf(stderr, "\n\n");
@@ -114,15 +109,20 @@ static void remove_mem_info(void *ptr) {
     }
     // update memstats
     memstats.num_allocations--;
-    memstats.current_allocated_size-=head->size_allocated;
+    memstats.current_allocated_size-=alias->size_allocated;
     for(int i = ALLOCATION_BUCKET_OFFSET; i < ALLOCATION_BUCKET_COUNT+ALLOCATION_BUCKET_OFFSET; i++) {
-        if(head->size_allocated < (1 << i)) {
+        if(alias->size_allocated < (1 << i)) {
             memstats.allocation_buckets[i-ALLOCATION_BUCKET_OFFSET]--;
             break;
         }
     }
+
     prior->next = alias->next;
     free(alias);
+
+    if(alias == head) {
+        head = NULL;
+    }
     alias = NULL;
     prior = NULL;
 }
@@ -161,16 +161,8 @@ static void append_mem_info(void *ptr, size_t size) {
     }
 }
 
-/* Call this instead of regular malloc for stats info
- * Every call should:
- * increase num_allocations
- * increase current_allocated_size
- * increment the corresponding allocation_buckets idx
- * TODO: figure out time logic
- */
+// Call this instead of regular malloc for stats info
 void *stats_malloc(size_t size) {
-    printf("special malloc\r\n");
-
     void * ret = (void *)malloc(size);
 
     // only update stats if malloc succeeds
@@ -183,11 +175,10 @@ void *stats_malloc(size_t size) {
 
 // Call this instead of regular realloc for stats info
 void *stats_realloc(void *ptr, size_t size) {
-    printf("special realloc\r\n");
-
     void * ret = (void *)realloc(ptr, size);
     // only update stats if realloc succeeds
     if(ret != NULL){
+        remove_mem_info(ptr);
         append_mem_info(ret, size);
     }
 
@@ -196,12 +187,10 @@ void *stats_realloc(void *ptr, size_t size) {
 
 // Call this instead of regular calloc for stats info
 void *stats_calloc(size_t nitems, size_t size) {
-    printf("special calloc\r\n");
-
     void * ret = (void *)calloc(nitems, size);
     // only update stats if calloc succeeds
     if(ret != NULL){
-        append_mem_info(ret, size);
+        append_mem_info(ret, nitems*size);
     }
 
     return ret;
@@ -209,8 +198,6 @@ void *stats_calloc(size_t nitems, size_t size) {
 
 // Call this instead of regular free for stats info
 void stats_free(void *ptr) {
-    printf("special free\r\n");
-
     remove_mem_info(ptr);
     free(ptr);
 }
--- mem_wrapper.h
+++ mem_wrapper.h
@@ -1,6 +1,30 @@
-#pragma once 
+#pragma once
 
 #include <stdlib.h>
+
+#define ALLOCATION_BUCKET_COUNT (12)
+#define ALLOCATION_BUCKET_OFFSET (2)
+#define TIME_BUCKET_COUNT (5)
+
+// global statistics
+typedef struct {
+    uint64_t num_allocations; // how many allocations have been made total
+    uint64_t current_allocated_size; // how much memory have we currently allocated
+    uint64_t allocation_buckets[ALLOCATION_BUCKET_COUNT]; // how much memory was allocated
+    uint64_t time_buckets[TIME_BUCKET_COUNT]; // how much time is memory spent allocated
+} mem_stats_t;
+
+// created each time memory is allocated, removed each time memory is freed
+typedef struct mem_info {
+    void * ptr;
+    uint64_t time_allocated;
+    uint64_t size_allocated;
+    struct mem_info *next;
+} mem_info_t;
+
+
+// Returns the mem_stats
+mem_stats_t get_mem_stats(void);
 
 // Call this instead of regular malloc for stats info
 void *stats_malloc(size_t size);
